0008cfc71db252ba67fcdac6fcb30b61d09e4fef
Marcin Kwiatek <kwiatekm4@gmail.com>
2021-03-03 20:08:05 +0100

    add click publish

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ function addPost() {
         `<div class="postBlog" id="addPost">
             <textarea class="addTitle" placeholder="Wpisz tytuł postu"></textarea>
             <textarea class="addContent" placeholder="Wpisz treść postu"></textarea>
-            <button class="publish">Opublikuj</button>
+            <button class="publish" id="publish">Opublikuj</button>
         </div>`)
-        let addPostButton = document.getElementById("addPostButton")
-        addPostButton.remove()
-    let postBlog = document.getElementById("postBlog")
-    let addTitle = document.getElementById("addTitle")
-    let addContent = document.getElementById("addContent")
-    
+    let addPostButton = document.getElementById("addPostButton")
+    addPostButton.remove()
+    let publish = document.getElementById("publish")
+    publish.addEventListener("click", function () { publishPost() })
+}
+function publishPost() {
+    let addPost = document.getElementById("addPost")
+    addPost.remove() 
+    let newTitle = document.createElement("div")
+    newTitle.innerHTML = "<h1>Hi there and greetings!</h1>";
 }
