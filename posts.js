e698e4e60219d9d10f121c2e29e740192d3d8def
Marcin <kwiatekm4@gmail.com>
2021-03-15 18:14:06 +0100

    arrangement title and content from local storage

--- posts.js
+++ posts.js
@@ -1,28 +1,37 @@
+  
 window.addEventListener('DOMContentLoaded', (event) => {
+    let titleArr = []
     for (let i = 0; i < localStorage.length; i++) {
-        let div = document.createElement("div")
         let key = localStorage.key(i)
-        div.innerHTML = localStorage.getItem(localStorage.key(i))
         if (key.startsWith("title")) {
-            div.style.width = "100%"
-            div.style.minHeight = "50px"
-            div.style.fontSize = "36px"
-            div.style.textAlign = "center"
-            div.style.color = "white"
-            div.style.fontWeight = "bold"
-            div.style.marginBottom = "20px"
-            div.style.marginRight = "auto"
-            
+            let keyNumber = key.substr(5)
+            titleArr.push(keyNumber)
         }
-        else{
-            div.style.width = "100%"
-            div.style.background = "lightgray"
-            div.style.color = "black"
-            div.style.padding = "10px"
-            div.style.fontSize = "22px"
-            div.style.marginBottom = "20px"
-            div.style.textAlign = "justify"
-        }
-        document.getElementById("title").appendChild(div)
+    }
+    for (let j = 0; j < titleArr.length; j++) {
+        let divTitle = document.createElement("div")
+        divTitle.innerHTML = localStorage.getItem("title" + titleArr[j])
+        let divContent = document.createElement("div")
+        divContent.innerHTML = localStorage.getItem("content" + titleArr[j])
+
+        divTitle.style.width = "100%"
+        divTitle.style.minHeight = "50px"
+        divTitle.style.fontSize = "36px"
+        divTitle.style.textAlign = "center"
+        divTitle.style.color = "white"
+        divTitle.style.fontWeight = "bold"
+        divTitle.style.marginBottom = "20px"
+        divTitle.style.marginRight = "auto"
+
+
+        divContent.style.width = "100%"
+        divContent.style.background = "lightgray"
+        divContent.style.color = "black"
+        divContent.style.padding = "10px"
+        divContent.style.fontSize = "22px"
+        divContent.style.marginBottom = "20px"
+        divContent.style.textAlign = "justify"
+        document.getElementById("title").appendChild(divTitle)
+        document.getElementById("title").appendChild(divContent)
     }
 })
