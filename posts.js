aeee7ba82bb8772e1d2022b751ea0068fd00f229
Marcin <kwiatekm4@gmail.com>
2021-04-05 14:41:27 +0200

    show the posts from database

--- posts.js
+++ posts.js
@@ -1,25 +1,17 @@
   window.addEventListener('DOMContentLoaded', (event) => {
       fetch('http://localhost:5000/allArticles')
           .then(function(response) { return response.json() })
-          .then(function(data) { console.log(data) })
-
-
-      /*let titleArr = []
-      for (let i = 0; i < localStorage.length; i++) {
-          let key = localStorage.key(i)
-          if (key.startsWith("title")) {
-              let keyNumber = key.substr(5)
-              titleArr.push(keyNumber)
-          }
-      }
-      for (let j = 0; j < titleArr.length; j++) {
-          let divTitle = document.createElement("div")
-          divTitle.innerHTML = localStorage.getItem("title" + titleArr[j])
-          divTitle.classList.add("divTitle")
-          let divContent = document.createElement("div")
-          divContent.innerHTML = localStorage.getItem("content" + titleArr[j])
-          divContent.classList.add("divContent")
-          document.getElementById("title").appendChild(divTitle)
-          document.getElementById("title").appendChild(divContent)
-      }*/
+          .then(function(data) {
+              console.log(data.data.length)
+              for (let i = 0; i < data.data.length; i++) {
+                  let divTitle = document.createElement("div")
+                  divTitle.innerHTML = data.data[i].title
+                  divTitle.classList.add("divTitle")
+                  let divContent = document.createElement("div")
+                  divContent.innerHTML = data.data[i].content
+                  divContent.classList.add("divContent")
+                  document.getElementById("title").appendChild(divTitle)
+                  document.getElementById("title").appendChild(divContent)
+              }
+          })
   })
